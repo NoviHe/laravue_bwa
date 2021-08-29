2a98559d94ad73e46d5cc32f2d0010687b3744fe
Novi Herlambang <claywhoami6@gmail.com>
2021-08-29 11:38:36 +0700

    cart screen & success screen

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -7,7 +7,7 @@
         <div class="row">
           <div class="col-lg-12">
             <div class="breadcrumb-text product-more">
-              <a href="./home.html"><i class="fa fa-home"></i> Home</a>
+              <router-link to="/"><i class="fa fa-home"></i> Home</router-link>
               <span>Detail</span>
             </div>
           </div>
@@ -98,8 +98,10 @@
                     <h4>$495.00</h4>
                   </div>
                   <div class="quantity">
-                    <a href="shopping-cart.html" class="primary-btn pd-cart"
-                      >Add To Cart</a
+                    <router-link to="/cart">
+                      <a href="shopping-cart.html" class="primary-btn pd-cart"
+                        >Add To Cart</a
+                      ></router-link
                     >
                   </div>
                 </div>
@@ -110,6 +112,9 @@
       </div>
     </section>
     <!-- Product Shop Section End -->
+
+    <RelatedShayna />
+
     <FooterShayna />
   </div>
 </template>
@@ -118,6 +123,7 @@
 // @ is an alias to /src
 import HeaderShayna from "@/components/HeaderShayna.vue";
 import FooterShayna from "@/components/FooterShayna.vue";
+import RelatedShayna from "@/components/RelatedShayna.vue";
 
 import carousel from "vue-owl-carousel";
 export default {
@@ -126,6 +132,7 @@ export default {
     HeaderShayna,
     FooterShayna,
     carousel,
+    RelatedShayna,
   },
   data() {
     return {
